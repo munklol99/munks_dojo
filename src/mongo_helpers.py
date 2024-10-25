48471cc4e159e12b7942197582624075a22aae46
Connor Dolan <Connor@UDigs-MacBook-Pro-5.local>
2024-10-25 10:31:32 -0400

    Added history updating and delete history when user is deleted

--- src/mongo_helpers.py
+++ src/mongo_helpers.py
@@ -32,7 +32,14 @@ def create_new_user(disc_username, lol_username, roles, pref_role):
         'Previous ELO': math.nan,
     }
     
+    user_history = {
+        'Discord Username': disc_username,
+        'Username': lol_username,
+        'Elo History': [config['account_creation']['default_elo']]
+    }
+    
     dojo_collection.insert_one(user)
+    history_collection.insert_one(user_history)
     
     rerank_users()
     
@@ -44,6 +51,7 @@ def delete_user(disc_username):
         return f"Username {disc_username} does not exists"
     
     dojo_collection.delete_many({'Discord Username': disc_username})
+    history_collection.delete_many({'Discord Username': disc_username})
     rerank_users()
     
     return "User deleted successfully"
@@ -60,19 +68,30 @@ def update_elo(disc_username, elo_change):
     if not user:
         return f"Username {disc_username} does not exists"
     
-    dojo_collection.update_one({"_id": disc_username}, {"$inc": {"Current Elo": elo_change}})
-    return "User elo updated successfully"
+    dojo_collection.update_one({"_id": user['_id']}, {"$inc": {"Current ELO": elo_change}})
+    add_to_elo_history(disc_username=disc_username, new_elo=user['Current ELO'] + elo_change)
     
+    rerank_users() # Take this out later
+    
+    return "User elo updated successfully"
+
 def update_users_elo(disc_usernames, elo_changes):
     for disc_username, elo_change in zip(disc_usernames, elo_changes):
         update_elo(disc_username=disc_username, elo_change=elo_change)
         
     rerank_users()
+    
+def add_to_elo_history(disc_username, new_elo):
+    history_collection.update_one(
+        {"Discord Username": disc_username},
+        {"$push": {"Elo History": new_elo}}
+    )
 
 if __name__ == '__main__':
     dbname = get_database()
     
     dojo_collection = dbname['userData']
+    history_collection = dbname['user_elo_history']
     
     items = dojo_collection.find({'Current ELO': {'$gt': 800}})
     # items = dojo_collection.find()
@@ -81,6 +100,8 @@ if __name__ == '__main__':
     # print(items)
     
     print(create_new_user('Test', 'LOL_Test', ['Top', 'Mid', 'Jungle', 'ADC', 'Support'], 'Jungle'))
+    
+    update_elo(disc_username='Test', elo_change=10)
     # items = dojo_collection.find({'Discord Username': 'Test'})
     items = dojo_collection.find()
     
